f4abd8f5fdec4d7b4dacec844c785996d9dbd302
Ava Bullock <ava.m.bullock@gmail.com>
2021-02-27 12:33:58 -0700

    added api fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-alert("Script working!")
+function weatherFetch(){
+    fetch(
+        // make a fetch request to the weather API
+        'https://api.openweathermap.org/data/2.5/weather?q='
+        +
+        // search term
+        'Salt Lake City&'
+        +
+        // api key
+        'appid=8bed639fd98c9152d287e65173b6a1c7'
+    )
+    .then(function(){
+        return
+    })
+}
+
+weatherFetch();
