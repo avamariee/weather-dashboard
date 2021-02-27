eb37104cb181b08c4cbc6dc4ba55ea8044b34ad5
Ava Bullock <ava.m.bullock@gmail.com>
2021-02-27 16:55:03 -0700

    got cities to appear on webpage and did wireframing for css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,52 @@
-function weatherFetch(){
+let cityName = document.querySelector("#city-name")
+let cityHistory = document.querySelector("#city-history")
+let weatherDisplay = document.getElementById("#weather-display")
+let searchButton = document.getElementById("search-button");
+
+function weatherFetch(city) {
+
     fetch(
         // make a fetch request to the weather API
         'https://api.openweathermap.org/data/2.5/weather?q='
         +
         // search term
-        'Salt Lake City&'
+        city
         +
         // api key
-        'appid=8bed639fd98c9152d287e65173b6a1c7'
+        '&appid=8bed639fd98c9152d287e65173b6a1c7'
     )
-    .then(function(){
-        return
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response);
+            console.log(response.name)
+
+            let citySearch = document.querySelector("#city-search")
+
+            citySearch.innerHTML = response.name;
+
+            // additional data
+
+        })
+
+
+    let cityResult = document.createElement('h2');
+
+
+
+
+
 }
 
-weatherFetch();
+function buttonPress() {
+
+    console.log(cityName.value)
+
+
+    weatherFetch(cityName.value);
+
+}
+
+searchButton.addEventListener("click", buttonPress)
+
