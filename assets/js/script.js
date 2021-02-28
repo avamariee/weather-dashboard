24b7222fc96450281a0102ace987f3f7933c65cb
Ava Bullock <ava.m.bullock@gmail.com>
2021-02-28 14:23:23 -0700

    displays temperature, humidity, wind and current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ function weatherFetch(city) {
         // search term
         city
         +
-        // api key
+        // key
         '&appid=8bed639fd98c9152d287e65173b6a1c7'
     )
         .then(function (response) {
@@ -24,16 +24,47 @@ function weatherFetch(city) {
 
             let citySearch = document.querySelector("#city-search")
 
-            citySearch.innerHTML = response.name;
+            citySearch.innerHTML = response.name + '  ' + new Date().toLocaleString();
 
             // additional data
 
+            let cityTemp = document.querySelector("#temperature")
+            cityTemp.innerHTML = "Temperature: " + response.main.temp
+
+            let cityHumidity = document.querySelector("#humidity")
+            cityHumidity.innerHTML = "Humidity: " + response.main.humidity
+
+            let cityWind = document.querySelector("#wind")
+            cityWind.innerHTML = "Wind speed: " + response.wind.speed + " miles per hour."
+
+            let cityWeather = document.querySelector("#weather")
+            cityWeather.innerHTML = "Current weather: " + response.weather[0].description
+
         })
 
+    fetch(
+        // make a fetch request for the 5-day forecast
+        "https://api.openweathermap.org/data/2.5/forecast?q="
+        +
+        // search term
+        city +
+        // key
+        "&appid=8bed639fd98c9152d287e65173b6a1c7"
+    )
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        console.log(response);
+        console.log(response.name)
 
-    let cityResult = document.createElement('h2');
+        // day one forecast
 
+        let dayOne = document.querySelector("#day-one")
+        dayOne.innerHTML = response.list[0].main
+    })
 
+   
 
 
 
